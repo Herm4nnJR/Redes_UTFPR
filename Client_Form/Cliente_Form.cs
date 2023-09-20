ddc66eb1f8892f04fb8a9f640b405cca6dc86ee8
Herm4nnJR <92889132+Herm4nnJR@users.noreply.github.com>
2023-09-19 21:21:14 -0300

    Trabalho inicial
    Primeira entrega do trabalho

---
+++ Client_Form/Cliente_Form.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_Form
+{
+    public partial class Cliente_Form : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        public Cliente_Form()
+        {
+            InitializeComponent();
+        }
+        private void Cliente_Form_Load(object sender, EventArgs e)
+        {
+            client = new TcpClient("127.0.0.1", 8081); // Endereço IP e porta do servidor
+            stream = client.GetStream();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Enviar dados para o servidor quando o botão for clicado
+            string mensagem = textBox1.Text;
+
+
+            byte[] mensagemBytes = Encoding.ASCII.GetBytes(mensagem);
+            try
+            {
+                stream.Write(mensagemBytes, 0, mensagemBytes.Length);
+
+                // Resposta do servidor
+                if (mensagem.ToLower() == "sair")
+                {
+                    this.Close();
+                }
+                else if (mensagem.Length > 9 && mensagem.Substring(0, 9).ToLower() == "arquivo: ")
+                {
+                    Thread.Sleep(1000);
+                    byte[] receivedHash = new byte[32]; // O hash SHA-256 tem 32 bytes
+                    stream.Read(receivedHash, 0, receivedHash.Length);
+                    Thread.Sleep(3000);
+                    byte[] fileNameBytes = new byte[1024];
+                    int bytesRead = stream.Read(fileNameBytes, 0, fileNameBytes.Length);
+                    string fileName = Encoding.UTF8.GetString(fileNameBytes, 0, bytesRead);
+
+                    //using (MemoryStream fileData = new MemoryStream())
+                    //{
+                        MemoryStream fileData = new MemoryStream();
+                        byte[] buffer = new byte[1024];
+                    Thread.Sleep(3000);
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                        fileData.Write(buffer, 0, bytesRead);
+
+
+                        using (SHA256 sha256 = SHA256.Create())
+                        {
+                            byte[] calculatedHash = sha256.ComputeHash(fileData.ToArray());
+
+                            if (StructuralComparisons.StructuralEqualityComparer.Equals(calculatedHash, receivedHash))
+                            {
+                                MessageBox.Show("Hash SHA-256 verificado com sucesso. Os dados do arquivo estão íntegros.");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Erro: O hash SHA-256 não corresponde. Os dados do arquivo podem estar corrompidos.");
+                            }
+                        }
+
+                        File.WriteAllBytes(fileName, fileData.ToArray());
+                        MessageBox.Show($"Arquivo salvo com sucesso: {fileName}");
+                    //}
+
+                }
+                else
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string resposta = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    MessageBox.Show("Resposta: " + resposta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao enviar: {ex.Message}");
+            }
+
+            // Limpa o TextBox
+            textBox1.Clear();
+        }
+
+        private Button button2;
+        private TextBox textBox1;
+
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(550, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Enviar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(613, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Cliente_Form
+            // 
+            this.ClientSize = new System.Drawing.Size(637, 253);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Cliente_Form";
+            this.Text = "Cliente";
+            this.Load += new System.EventHandler(this.Cliente_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+    }
+}
+
---
+++ Server_Network/Server.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace Server_Network
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 8081;
+
+            TcpListener server = new TcpListener(ip, port);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine($"Server on {ip}:{port}");
+                while (true)
+                {
+                    TcpClient clientSocket = server.AcceptTcpClient();
+                    Console.WriteLine($"Accepted connection from {((IPEndPoint)clientSocket.Client.RemoteEndPoint).Address}:{((IPEndPoint)clientSocket.Client.RemoteEndPoint).Port}");
+                    Thread clientThread = new Thread(() => HandleClient(clientSocket));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    
+        static void HandleClient(TcpClient clientSocket)
+        {
+            try
+            {
+                NetworkStream networkStream = clientSocket.GetStream();
+                byte[] receiveBuffer = new byte[1024];
+
+                Boolean boolLoop = true;
+                while (boolLoop)
+                {
+                    int bytesRead = networkStream.Read(receiveBuffer, 0, receiveBuffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        Console.WriteLine("Client disconected");
+                        break;
+                    }
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBuffer, 0, bytesRead);
+                    Console.WriteLine($"Received: {receivedData}");
+                    VerifyContent(clientSocket,networkStream, receivedData);
+                }
+
+                clientSocket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao enviar: {ex.Message}");
+            }
+        }
+
+        static bool VerifyContent(TcpClient clientSocket, NetworkStream stream, string msg)
+        {
+            if (msg.ToLower() == "sair")
+            {
+                return false;
+            }
+            else if (msg.Length > 9 && msg.Substring(0,9).ToLower() == "arquivo: ")
+            {
+                string nomeArquivo = msg.Substring(9);
+                Console.WriteLine($"Enviando arquivo: {nomeArquivo}");
+
+                using (FileStream fileStream = File.OpenRead(nomeArquivo))
+                {
+                    using (SHA256 hash = SHA256.Create())
+                    {
+                        byte[] fileHash = hash.ComputeHash(fileStream);
+                        stream.Write(fileHash, 0, fileHash.Length);
+                    }
+                    fileStream.Seek(0, SeekOrigin.Begin);
+                    Thread.Sleep(3000);
+                    // Enviar o nome do arquivo
+                    byte[] fileNameBytes = Encoding.UTF8.GetBytes(nomeArquivo);
+                    stream.Write(fileNameBytes, 0, fileNameBytes.Length);
+
+                    // Envia os dados
+                    byte[] buffer = new byte[1024];
+                    //int bytesRead;
+                    //while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                    //{
+                    Thread.Sleep(3000);
+                    int bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+                    stream.Write(buffer, 0, bytesRead);
+                    //}
+                    // Enviar um sinal de término
+                    //stream.Write(buffer, 0, 0);
+                }
+
+                Console.WriteLine("Arquivo enviado com sucesso!");
+                return true;
+            }
+            else
+            {
+                byte[] sendData = Encoding.ASCII.GetBytes(msg);
+                stream.Write(sendData, 0, sendData.Length);
+
+                return true;
+            }
+        }
+    }
+}
